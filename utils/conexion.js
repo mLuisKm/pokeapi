6a3947ee2f1efde5e903da375c7eaf5c3cc7d882
Luis7km <mluis.km@gmail.com>
2025-08-02 20:17:40 -0500

    Modified gitignore and supabase keys got hidden

--- utils/conexion.js
+++ utils/conexion.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://imjutjbqyrjxlklfsnie.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltanV0amJxeXJqeGxrbGZzbmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NjY0NjcsImV4cCI6MjA2OTE0MjQ2N30.1y7ewVhETHBLS8Z6C4dYdXOCnlMGlrfRA6Tam7S3KdA'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
